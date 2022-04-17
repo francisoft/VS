37dce772a7f258d18e32c3173596d0c9f496cd7b
francisoft <suportefgtec@gmail.com>
2022-04-16 21:45:43 -0300

    inicio das alterações

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cripto_moedas/configs/app_settings.dart';
+import 'package:cripto_moedas/configs/hive_config.dart';
+import 'package:cripto_moedas/repositories/conta_repository.dart';
+import 'package:cripto_moedas/repositories/favoritas_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'configs/app_settings.dart';
+import 'configs/hive_config.dart';
+import 'meu_aplicativo.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ContaRepository()),
+        ChangeNotifierProvider(create: (context) => AppSettings()),
+        ChangeNotifierProvider(create: (context) => FavoritasRepository()),
+      ],
+      child: MeuAplicativo(),
+    ),
+  );
+}
